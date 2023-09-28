3b183654fb9298170aa9f7e4af8d477b50f8d4ec
Lijo Joseph <lijojoseph234@gmail.com>
2023-09-28 21:03:37 +0530

    day4 final

---
+++ Demo/JavaSrc/G12Java/src/com/lj/java/Main.java
@@ -0,0 +1,30 @@
+package com.lj.java;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+//		// output
+//		System.out.println("lijo");
+//		
+//		// integer 
+//		int num = 23;
+//		//float
+//		float num1 = 23.67f;
+//		// double
+//		double num2 = 86576576.67867687;
+//		
+//		//boolean
+//		boolean isCheck = true;
+//		
+//		//input
+		Scanner input = new Scanner(System.in);
+//		int a = input.nextInt();
+		
+		
+		int a = 0;
+		System.out.println(a);
+		
+		
+	}
+}
