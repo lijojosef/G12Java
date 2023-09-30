874f9e69dad5c7c2970cf8870e883622b4a7e9f8
Lijo Joseph <lijojoseph234@gmail.com>
2023-09-30 20:48:46 +0530

    day5 complete

---
+++ Demo/JavaSrc/G12Java/src/com/lj/basics/Swap.java
@@ -0,0 +1,49 @@
+package com.lj.basics;
+
+public class Swap {
+
+	 int value = 10;
+	
+	public static void main(String[] args) {
+		int a = 5; 
+		int b = 6;
+		
+//		swap(a, b);
+		
+//		value = value + 1;
+		
+		
+		System.out.println(a + " " + b);
+//		num1 + num2;
+		
+		{
+//			int ans = a + b;
+			int d = 7;
+			
+		}
+		
+//		System.out.println(d);
+		
+		
+		for (int i = 0; i < a; i++) {
+			//do something
+			
+			int z = 8;
+		}
+	}
+
+	 void swap(int num1, int num2) {
+		// TODO Auto-generated method stub
+		
+		int temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
+		int value = 8;
+		
+		System.out.println(value);
+//		a + b;
+//		System.out.println(num1 + " " + num2);
+		
+	}
+}
